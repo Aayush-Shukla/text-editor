159c13ef2de34b5b7c20079009539273b1597116
Ayush Shukla <34869001+Aayush-Shukla@users.noreply.github.com>
2020-08-20 12:37:49 +0530

    feat: undo

--- main.js
+++ main.js
@@ -1,12 +1,39 @@
-addEventListener("mousemove",getSelectionText)
-function getSelectionText() {
-    var text = "";
-    if (window.getSelection) {
-        text = window.getSelection().toString();
-    } else if (document.selection && document.selection.type != "Control") {
-        text = document.selection.createRange().t;
+var snaps=[]
+count=1
+var tm=''
+
+    function savetimer() {
+    // console.log("in")
+    tm=setTimeout(save, 2000)
+}
+
+
+document.getElementById("editor1").addEventListener("input", savetimer, false);
+function save() {
+    clearTimeout(tm)
+    count=1
+    // console.log(snaps.length)
+    if (snaps.length>=10){
+        snaps.splice(0,1)
+
+
     }
-    // console.log(window.getSelection());
+    var temp=document.getElementById("editor1").innerHTML
+    if(temp!=snaps[snaps.length-1]) {
+        // console.log(temp,snaps[snaps.length])
+
+
+        snaps.push(temp)
+    }
+    // console.log(snaps)
+}
+
+function undo(){
+    if(snaps.length-count-1>=0 ) {
+        console.log(snaps.length-count,snaps[snaps.length-count-1])
+        document.getElementById("editor1").innerHTML = snaps[snaps.length-count-1]
+        count++
+    }
 }
 function bolder(){
 
@@ -27,15 +54,13 @@ function bolder(){
     else {
 
 
-        // text.bold()
-        // document.getElementById("editor1").innerHTML = text
         console.log(text)
         var newst = newstring(window.getSelection().anchorNode.data, window.getSelection().anchorOffset, window.getSelection().focusNode.data, window.getSelection().focusOffset,text, window.getSelection().focusNode.data, tag)
-        // newText=newst.split(text).join()
+
         document.getElementById("editor1").innerHTML = newst
     }
 
-    // console.log(substring)
+
 }function italicize(){
     var tag='i'
     console.log(window.getSelection().focusNode.data,window.getSelection().focusOffset,window.getSelection().anchorNode.data,window.getSelection().anchorOffset);
@@ -119,29 +144,24 @@ function fontchange(){
     console.log(font)
     editor.style.fontFamily=font
     console.log(editor.style.fontFamily)
-    // var newfont=fontChange(window.getSelection().anchorNode.data,window.getSelection().anchorOffset,window.getSelection().focusNode.data,window.getSelection().focusOffset,text,font)
-    // newstring=text.split(substring).join("<b>"+substring+"</b>")
-    // document.getElementById("editor1").innerHTML=newfont
+
 }
 function newstring(anchorText,anchorIndex,focusText,focusIndex,inner, selectedtext, tag){
     console.log(inner,selectedtext)
-    // newanchorText=anchorText.bold()
-    // selectedtext=selectedtext.split(anchorText).join(newanchorText
 
 
     var tagEnd=`</${tag}>`
     var tagStart=`<${tag}>`
 
     if(anchorText==focusText){
-        // var newText=tagStart+selectedtext+tagEnd
+
         var end = [selectedtext.slice(0, anchorIndex), tagEnd, selectedtext.slice(anchorIndex)].join('');
         console.log(end)
-        // inner=inner.split(selectedtext).join(newText)
-        // console.log(inner)
+
         var start= [end.slice(0, focusIndex), tagStart, end.slice(focusIndex)].join('');
         console.log(start)
         inner=inner.split(selectedtext).join(start)
-        // console.log(inner,"-----------------",start)
+
 
         return (inner)
     }
@@ -154,7 +174,7 @@ else{
             inner=inner.split(anchorText).join(aoutput)
 
             var foutput=[focusText.slice(0, focusIndex), tagStart, focusText.slice(focusIndex)].join('');
-           // focusText=focusText.slice(0,focusIndex-1)
+
             console.log(focusText,foutput)
             inner=inner.split(focusText).join(foutput)
 
